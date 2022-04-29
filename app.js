3f1868cccdfe612aa3bef6976978545a953be48b
Xiao <xmeng1@ualberta.ca>
2022-04-29 17:46:53 -0600

    add Book constructor to app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+
+function Book(author, title, numberOfPages, read) {
+    this.author = author;
+    this.title = title;
+    this.numberOfPages = numberOfPages;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+    
+}
