70cbb3a1fdef4c9b3f54be6de8dbc4c8e8892f98
Xiao <xmeng1@ualberta.ca>
2022-05-01 15:48:47 -0600

    add creatBookElement and addBookElementToDOM featuers

--- app.js
+++ app.js
@@ -4,32 +4,27 @@ const authorInput = document.querySelector("#author-input");
 const pagesInput = document.querySelector("#pages-input");
 const readInput = document.querySelector("#read-input");
 
-handleEvents();
+addButton.addEventListener("click", addBookEvent);
 
 let myLibrary = [];
 
-function handleEvents() {
-    addButton.addEventListener("click", addBookToLibrary);
-}
-
-function Book(title, author, numberOfPages, read) {
-    this.title = title;
-    this.author = author;
-    this.numberOfPages = numberOfPages;
-    this.read = read;
-}
-
-function addBookToLibrary() {
+function addBookEvent() {
     const formFilled = validateNewBookForm();
 
     if (formFilled === false) {
         return;
     }
 
+    addBookToLibrary();
+    clearNewBookInputs();
+    bookElement = createBookElement();
+    addBookElementToDOM(bookElement);
+}
+
+function addBookToLibrary() {
     const newBook = new Book(titleInput.value, authorInput.value, 
                                 pagesInput.value, readInput.value);
     myLibrary.push(newBook);
-    clearNewBookInputs();
 }
 
 function validateNewBookForm() {
@@ -41,5 +36,51 @@ function clearNewBookInputs () {
     titleInput.value = "";
     authorInput.value = "";
     pagesInput.value = "";
-    readInput.value = "";
+    readInput.checked = false;
+}
+
+function createBookElement() {
+    const book = document.createElement("div");
+    const title = document.createElement("h2");
+    const author = document.createElement("h3");
+    const pages = document.createElement("h4");
+    const labelCheckboxContainer = document.createElement("div");
+    const label = document.createElement("label");
+    const input = document.createElement("input");
+    const button = document.createElement("button");
+    
+    labelCheckboxContainer.appendChild(label);
+    labelCheckboxContainer.appendChild(input);
+    book.appendChild(title);
+    book.appendChild(author);
+    book.appendChild(pages);
+    book.appendChild(labelCheckboxContainer);
+    book.appendChild(button);
+
+    book.dataset.bookNumber = "index";
+    book.classList = "book border";
+    title.textContent = "Title";
+    author.textContent = "Author";
+    pages.textContent = "x page";
+    labelCheckboxContainer.classList = "label-checkbox-container";
+    label.textContent = "Read?";
+    label.setAttribute("for", "read-book-index");
+    input.setAttribute("type", "checkbox");
+    input.setAttribute("id", "read-book-index");
+    button.textContent = "Remove";
+
+    return book;
+}
+
+function addBookElementToDOM(bookElement) {
+    const booksContainer = document.querySelector("#books-container");
+    booksContainer.appendChild(bookElement);
+}
+
+
+function Book(title, author, numberOfPages, read) {
+    this.title = title;
+    this.author = author;
+    this.numberOfPages = numberOfPages;
+    this.read = read;
 }
